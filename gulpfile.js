5e65be44fba8f0f22db3c154efce183513345c56
Jon Szeto <jonszeto821@gmail.com>
2019-01-02 21:01:59 -0500

    create task to optimize images

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('sass', () => {
   return gulp.src('src/scss/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('public/css'));
 })
+
+//Optimize Images
+gulp.task('imageMin', () => {
+  return gulp.src('src/images/screenshots/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('public/images/screenshots'));
+});
