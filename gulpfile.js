33d2711f0c6ec0ba9e9cd9a17a782d03f59bffa7
Jon Szeto <jonszeto821@gmail.com>
2019-01-04 14:18:59 -0500

    update gulp task functions to Gulp 4 convention

--- gulpfile.js
+++ gulpfile.js
@@ -5,32 +5,38 @@ const concat = require('gulp-concat');
 const terser = require('gulp-terser');
 const cleanCSS = require('gulp-clean-css');
 
-gulp.task('sass', () => {
-  return gulp.src('src/scss/style.scss')
+//Minify Sass
+function sassMin(cb) {
+  gulp.src('src/scss/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
     .pipe(gulp.dest('public/css'));
-})
+  cb();
+}
 
 //Optimize Images
-gulp.task('imageMin', () => {
-  return gulp.src('src/images/screenshots/*')
+function imageOpt(cb) {
+  gulp.src('src/images/screenshots/*')
     .pipe(imagemin())
     .pipe(gulp.dest('public/images/screenshots'));
-});
+  cb();
+}
 
-//compile & minify js files to one files
-gulp.task('scripts', () => {
-  return gulp.src('src/javascript/*.js')
+//Minify Js
+function jsMin(cb) {
+  gulp.src('src/javascript/*.js')
     .pipe(concat('main.js'))
     .pipe(terser())
     .pipe(gulp.dest('public/javascript'));
-});
+  cb();
+}
 
-gulp.task('default', gulp.series(['sass', 'imageMin', 'scripts']));
+gulp.task('minify', gulp.parallel(sassMin, jsMin));
+
+gulp.task('default', gulp.series('minify', imageOpt));
 
 gulp.task('watch', () => {
-  gulp.watch('src/scss/*.scss', gulp.series('sass'));
-  gulp.watch('src/images/screenshots/*', gulp.series('imageMin'));
-  gulp.watch('src/javascript/*.js', gulp.series('scripts'));
+  gulp.watch('src/scss/*.scss', gulp.series(sassMin));
+  gulp.watch('src/images/screenshots/*', gulp.series(imageOpt));
+  gulp.watch('src/javascript/*.js', gulp.series(jsMin));
 });
